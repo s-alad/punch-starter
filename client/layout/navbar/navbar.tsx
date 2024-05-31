8e1fc038dfd7eeaf28a58e416138a66f9d8e75dd
s-alad <n7.saad@gmail.com>
2024-05-31 13:26:03 -0400

    fix

--- client/layout/navbar/navbar.tsx
+++ client/layout/navbar/navbar.tsx
@@ -1,15 +1,4 @@
-import {
-  Box,
-  Button,
-  Flex,
-  Image,
-  Input,
-  InputGroup,
-  InputLeftAddon,
-  useBreakpointValue,
-  useColorModeValue,
-  useTheme,
-} from "@chakra-ui/react";
+
 import { useEffect, useState } from "react";
 import { FaHandPointer, FaSearch, FaUser, FaPlus } from "react-icons/fa";
 import s from "./navbar.module.scss"
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,10 +1,6 @@
-import ConnectWallet, {
-  DeployContract,
-} from "@/sdk-connection-code/stacks-connection";
-
-//import ChatUI from "@/components/Chat";
 import Head from "next/head";
 import Image from "next/image";
+import s from "./index.module.scss";
 import Top from "@/components/top/top";
 import Rising from "@/components/rising/rising";
 
@@ -12,15 +8,13 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Punchstarter</title>
+        <title>Lift</title>
         <meta name="description" content="lift" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-
-      <main>
-        <div>
+      <main className={`${s.main}`}>
+        <div className={s.left}>
           <Top />
         </div>
         <div className={s.right}>
